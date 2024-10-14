312b009bc217f48db32ca8dc1c614c41cb8e040b
quentin vernaison <quentin.vernaison@saintmichelannecy.fr>
2024-10-14 18:45:59 +0200

    AvancerDesign

--- ProjetAdminSmartDisplay/MainWindow.xaml.cs
+++ ProjetAdminSmartDisplay/MainWindow.xaml.cs
@@ -6,48 +6,53 @@ namespace ProjetAdminSmartDisplay
 {
     public partial class MainWindow : Window
     {
+
+        private object _initialContent;
         public MainWindow()
         {
             InitializeComponent();
             //ouvrir en plein ecran
             WindowState = WindowState.Maximized;
+
+            // Sauvegarder le contenu initial au démarrage
+            _initialContent = MainContentControl.Content;
         }
 
         private void EcranButton_Click(object sender, RoutedEventArgs e)
         {
             // Charger EcranView dans le ContentControl et masquer le logo et le titre
             MainContentControl.Content = new EcranView();
-            LogoStackPanel.Visibility = Visibility.Collapsed;
+            
         }
 
         private void EnvoyerDocumentButton_Click(object sender, RoutedEventArgs e)
         {
             // Charger EnvoyerDocumentView dans le ContentControl et masquer le logo et le titre
             MainContentControl.Content = new EnvoyerDocumentView();
-            LogoStackPanel.Visibility = Visibility.Collapsed;
+            
         }
 
         private void AddClasse_Click(object sender, RoutedEventArgs e)
         {
             // Charger une autre vue et masquer le logo et le titre
             MainContentControl.Content = new AddClasse();
-            LogoStackPanel.Visibility = Visibility.Collapsed;
+            
         }
 
         private void SupprimerLesSalles_Click(object sender, RoutedEventArgs e)
         {
             // Charger une autre vue et masquer le logo et le titre
             MainContentControl.Content = new SupprimerLesSalles();
-            LogoStackPanel.Visibility = Visibility.Collapsed;
+            
         }
 
 
 
         private void HomeButton_Click(object sender, RoutedEventArgs e)
         {
-            // Vider le contenu du ContentControl et réafficher le logo et le titre
-            MainContentControl.Content = null;
-            LogoStackPanel.Visibility = Visibility.Visible; // Afficher le logo et le titre
+            // Restaurer le contenu initial
+            MainContentControl.Content = _initialContent;
+
         }
     }
 }
